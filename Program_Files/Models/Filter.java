7bc7aa7b6e0ba75fa1414d1a4f26ada39e947529
Tyler Tran <haxkue@gmail.com>
2021-12-04 14:40:49 -0700

    Filter framwork

--- Program_Files/Models/Filter.java
+++ Program_Files/Models/Filter.java
@@ -1,17 +1,41 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import Interfaces.PropertyType;
+
 public class Filter {
-  private String propertyType;
+  private PropertyType propertyType;
   private String propertyQuad;
   private int minBedroom;
   private int maxBedroom;
   private boolean isFurnished;
 
-  public Filter(String propertyType, String propertyQuad, int minBedroom, int maxBedroom, boolean isFurnished) {
+  public Filter(PropertyType propertyType, String propertyQuad, int minBedroom, int maxBedroom, boolean isFurnished) {
     this.propertyType = propertyType;
     this.propertyQuad = propertyQuad;
     this.minBedroom = minBedroom;
     this.maxBedroom = maxBedroom;
     this.isFurnished = isFurnished;
+  }
+
+  // Not sure if we'll use
+  public ArrayList<Property> filterAll(ArrayList<Property> properties)
+  {
+    return new ArrayList<Property>(
+      properties.stream()
+      .filter((prop) -> check(prop))
+      .collect(Collectors.toList()));
+  }
+
+  // ===============================================
+  // TODO
+  // For Noel to finish (return false if property does not match search criteria)
+  // Null or -1 means do not check!
+  // ===============================================
+  public boolean check(Property property)
+  {
+    return true;
   }
 }
