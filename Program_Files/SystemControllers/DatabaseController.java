719a3249a033ed48d372bbd1e21cdf730556979f
sinatm3000 <81539465+sinatm3000@users.noreply.github.com>
2021-12-09 21:22:07 -0700

    Update DatabaseController.java

--- Program_Files/SystemControllers/DatabaseController.java
+++ Program_Files/SystemControllers/DatabaseController.java
@@ -148,7 +148,7 @@ public final class DatabaseController {
         ArrayList<Property> periodProperties= new ArrayList<Property>();
         try{
             Statement myStmt = database.createStatement();
-            ResultSet myRs = myStmt.executeQuery("select * from properties where properties.`date listed`>=\""+start+"\" and properties.`date listed` <=\""+end+"\"");
+            ResultSet myRs = myStmt.executeQuery("select * from properties join accounts on properties.account_id = accounts.account_id where properties.`date listed`>=\""+start+"\" and properties.`date listed` <=\""+end+"\"");
             while (myRs.next()){
                 Property selectedProperty = new Property();
                 selectedProperty.setPropertyId(myRs.getInt("property_id"));
